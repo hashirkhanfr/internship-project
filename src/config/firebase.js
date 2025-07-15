afc4e679e534312502e23a93b9840f2956619311
Mansoorysfxi <hashirkhanfr@gmail.com>
2025-07-15 16:29:11 +0500

    Set up firebase and firebase-CLI

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyjBgzQCmWcnCOlU5GYd2wlyDAJGKdq8c",
+  authDomain: "internship-2025-2.firebaseapp.com",
+  projectId: "internship-2025-2",
+  storageBucket: "internship-2025-2.firebasestorage.app",
+  messagingSenderId: "760544131744",
+  appId: "1:760544131744:web:2d381e4e41237938a0a7c5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
